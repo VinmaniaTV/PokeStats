ca166c2cda46a3c2545849e1458a7ce288fef28a
Vinmania <vinmaniamc@gmail.com>
2021-01-10 17:30:20 +0100

    Page de détail d'un pokémon

--- client/components/Pokemon.vue
+++ client/components/Pokemon.vue
@@ -16,10 +16,13 @@
         <div
           class="pokemon-image"
           v-if="
-                pokedex[pokedex.findIndex((i) => i.id == $route.params.id)].image != null &&
-                pokedex[pokedex.findIndex((i) => i.id == $route.params.id)].image != 'null' &&
-                pokedex[pokedex.findIndex((i) => i.id == $route.params.id)].image != ''
-              "
+            pokedex[pokedex.findIndex((i) => i.id == $route.params.id)].image !=
+              null &&
+            pokedex[pokedex.findIndex((i) => i.id == $route.params.id)].image !=
+              'null' &&
+            pokedex[pokedex.findIndex((i) => i.id == $route.params.id)].image !=
+              ''
+          "
           :style="{
             backgroundImage:
               'url(' +
@@ -29,132 +32,141 @@
           }"
         ></div>
         <div
-              class="pokemon-image"
-              v-else
-              :style="{ backgroundImage: 'url(../img/unknownPokemon.png)' }"
-            ></div>
+          class="pokemon-image"
+          v-else
+          :style="{ backgroundImage: 'url(../img/unknownPokemon.png)' }"
+        ></div>
         <button
-        @click="
-          editPokemon(
-            pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
-          )
-        "
-      >
-        Modifier les informations
-      </button>
+          @click="
+            editPokemon(
+              pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
+            )
+          "
+        >
+          Modifier les informations
+        </button>
       </article>
       <aside>
         <div class="pokemon-no">
           <h4>No. Pokédex</h4>
-          <h4>
+          <h5>
             {{ pokedex[pokedex.findIndex((i) => i.id == $route.params.id)].no }}
-          </h4>
+          </h5>
         </div>
         <div class="pokemon-type">
           <h4>Type</h4>
-          <div
-            class="type"
-            :style="{
-              backgroundImage:
-                'url(\'../img/type/' +
+          <div class="pokemon-type-img">
+            <div
+              class="type"
+              :style="{
+                backgroundImage:
+                  'url(\'../img/type/' +
+                  pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
+                    .type1 +
+                  '.png\')',
+              }"
+            ></div>
+            <div
+              class="type"
+              v-if="
                 pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
-                  .type1 +
-                '.png\')',
-            }"
-          ></div>
-          <div
-            class="type"
-            v-if="
-              pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
-                .type2 !== null
-            "
-            :style="{
-              backgroundImage:
-                'url(\'../img/type/' +
-                pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
-                  .type2 +
-                '.png\')',
-            }"
-          ></div>
+                  .type2 !== null
+              "
+              :style="{
+                backgroundImage:
+                  'url(\'../img/type/' +
+                  pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
+                    .type2 +
+                  '.png\')',
+              }"
+            ></div>
+          </div>
         </div>
         <div class="pokemon-stats">
           <div class="pokemon-hp">
             <h4>HP</h4>
-            <h4>
+            <h5>
               {{
                 pokedex[pokedex.findIndex((i) => i.id == $route.params.id)].hp
               }}
-            </h4>
+            </h5>
           </div>
           <div class="pokemon-attack">
             <h4>Attaque</h4>
-            <h4>
+            <h5>
               {{
                 pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
                   .attack
               }}
-            </h4>
+            </h5>
           </div>
           <div class="pokemon-defense">
             <h4>Défense</h4>
-            <h4>
+            <h5>
               {{
                 pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
                   .defense
               }}
-            </h4>
+            </h5>
           </div>
           <div class="pokemon-spatk">
             <h4>Atk.Spé</h4>
-            <h4>
+            <h5>
               {{
                 pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
                   .spatk
               }}
-            </h4>
+            </h5>
           </div>
           <div class="pokemon-spdef">
             <h4>Déf.spé</h4>
-            <h4>
+            <h5>
               {{
                 pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
                   .spdef
               }}
-            </h4>
+            </h5>
           </div>
           <div class="pokemon-speed">
             <h4>Vitesse</h4>
-            <h4>
+            <h5>
               {{
                 pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
                   .speed
               }}
-            </h4>
+            </h5>
           </div>
           <div class="pokemon-generation">
             <h4>Génération</h4>
-            <h4>
+            <h5>
               {{
                 pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
                   .generation
               }}
-            </h4>
+            </h5>
           </div>
           <div class="pokemon-legendary">
             <h4>Légendaire</h4>
-            <h4>
+            <h5>
               {{
                 pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
                   .legendary
                   ? "Oui"
                   : " Non"
               }}
-            </h4>
+            </h5>
           </div>
         </div>
         <div class="pokemon-description">
           <h4>Description</h4>
-          <p>
+          <p
+            v-if="
+              pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
+                .description !== null &&
+              pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
+                .description !== ''
+            "
+          >
             {{
               pokedex[pokedex.findIndex((i) => i.id == $route.params.id)]
                 .description
@@ -170,6 +182,14 @@
         </div>
         <div
           class="pokemon-image"
+          v-if="
+            pokedex[pokedex.findIndex((i) => i.id == $route.params.id)].image !=
+              null &&
+            pokedex[pokedex.findIndex((i) => i.id == $route.params.id)].image !=
+              'null' &&
+            pokedex[pokedex.findIndex((i) => i.id == $route.params.id)].image !=
+              ''
+          "
           :style="{
             backgroundImage:
               'url(' +
@@ -184,6 +204,19 @@
             placeholder="Lien vers l'image"
           />
         </div>
+        <div
+          class="pokemon-image"
+          v-else
+          :style="{ backgroundImage: 'url(../img/unknownPokemon.png)' }"
+        >
+          <input
+            type="text"
+            v-model="editingPokemon.image"
+            placeholder="Lien vers l'image"
+          />
+        </div>
+        <button @click="sendEditPokemon()">Valider</button>
+        <button @click="abortEditPokemon()">Annuler</button>
       </article>
       <aside>
         <div class="pokemon-no">
@@ -192,47 +225,57 @@
         </div>
         <div class="pokemon-type">
           <h4>Type</h4>
-          <select name="type1" id="type1-select" v-model="editingPokemon.type1">
-            <option value="Bug">Insecte</option>
-            <option value="Dark">Ténèbres</option>
-            <option value="Dragon">Dragon</option>
-            <option value="Electric">Electrik</option>
-            <option value="Fairy">Fée</option>
-            <option value="Fighting">Combat</option>
-            <option value="Fire">Feu</option>
-            <option value="Flying">Vol</option>
-            <option value="Ghost">Spectre</option>
-            <option value="Grass">Plante</option>
-            <option value="Ground">Sol</option>
-            <option value="Ice">Glace</option>
-            <option value="Normal">Normal</option>
-            <option value="Poison">Poison</option>
-            <option value="Psychic">Psy</option>
-            <option value="Rock">Roche</option>
-            <option value="Steel">Acier</option>
-            <option value="Water">Eau</option>
-          </select>
-          <select name="type2" id="type2-select" v-model="editingPokemon.type2">
-            <option value="">Aucun</option>
-            <option value="Bug">Insecte</option>
-            <option value="Dark">Ténèbres</option>
-            <option value="Dragon">Dragon</option>
-            <option value="Electric">Electrik</option>
-            <option value="Fairy">Fée</option>
-            <option value="Fighting">Combat</option>
-            <option value="Fire">Feu</option>
-            <option value="Flying">Vol</option>
-            <option value="Ghost">Spectre</option>
-            <option value="Grass">Plante</option>
-            <option value="Ground">Sol</option>
-            <option value="Ice">Glace</option>
-            <option value="Normal">Normal</option>
-            <option value="Poison">Poison</option>
-            <option value="Psychic">Psy</option>
-            <option value="Rock">Roche</option>
-            <option value="Steel">Acier</option>
-            <option value="Water">Eau</option>
-          </select>
+          <div class="pokemon-type-select">
+            <select
+              name="type1"
+              id="type1-select"
+              v-model="editingPokemon.type1"
+            >
+              <option value="Bug">Insecte</option>
+              <option value="Dark">Ténèbres</option>
+              <option value="Dragon">Dragon</option>
+              <option value="Electric">Electrik</option>
+              <option value="Fairy">Fée</option>
+              <option value="Fighting">Combat</option>
+              <option value="Fire">Feu</option>
+              <option value="Flying">Vol</option>
+              <option value="Ghost">Spectre</option>
+              <option value="Grass">Plante</option>
+              <option value="Ground">Sol</option>
+              <option value="Ice">Glace</option>
+              <option value="Normal">Normal</option>
+              <option value="Poison">Poison</option>
+              <option value="Psychic">Psy</option>
+              <option value="Rock">Roche</option>
+              <option value="Steel">Acier</option>
+              <option value="Water">Eau</option>
+            </select>
+            <select
+              name="type2"
+              id="type2-select"
+              v-model="editingPokemon.type2"
+            >
+              <option value="">Aucun</option>
+              <option value="Bug">Insecte</option>
+              <option value="Dark">Ténèbres</option>
+              <option value="Dragon">Dragon</option>
+              <option value="Electric">Electrik</option>
+              <option value="Fairy">Fée</option>
+              <option value="Fighting">Combat</option>
+              <option value="Fire">Feu</option>
+              <option value="Flying">Vol</option>
+              <option value="Ghost">Spectre</option>
+              <option value="Grass">Plante</option>
+              <option value="Ground">Sol</option>
+              <option value="Ice">Glace</option>
+              <option value="Normal">Normal</option>
+              <option value="Poison">Poison</option>
+              <option value="Psychic">Psy</option>
+              <option value="Rock">Roche</option>
+              <option value="Steel">Acier</option>
+              <option value="Water">Eau</option>
+            </select>
+          </div>
         </div>
         <div class="pokemon-stats">
           <div class="pokemon-hp">
@@ -277,8 +320,6 @@
           <textarea v-model="editingPokemon.description"> </textarea>
         </div>
       </aside>
-      <button @click="sendEditPokemon()">Valider</button>
-      <button @click="abortEditPokemon()">Annuler</button>
     </section>
   </div>
 </template>
@@ -370,13 +411,12 @@ module.exports = {
 
 <style scoped>
 .main {
-  height: calc(100vh - 236px);
   background-color: #bfbfbf;
 }
 
 .pokemon-info-banner {
-  background-color: #64A4D8;
-  border-bottom: 2px solid #4888C0;
+  background-color: #64a4d8;
+  border-bottom: 2px solid #4888c0;
 }
 
 .pokemon-info-banner h2 {
@@ -424,11 +464,11 @@ aside {
 }
 
 aside div:nth-child(odd) {
-  background-color: #94C49C;
+  background-color: #94c49c;
 }
 
 aside div:nth-child(even) {
-  background-color: #84AC88;
+  background-color: #84ac88;
 }
 
 .pokemon-no,
@@ -442,11 +482,89 @@ aside div:nth-child(even) {
 .pokemon-generation,
 .pokemon-legendary {
   display: flex;
+  justify-content: space-between;
 }
 
 .type {
   width: 70px;
   height: 15px;
   align-self: center;
+}
+
+section .pokemon-type-img {
+  display: flex;
+  width: 140px;
+  margin-right: 15px;
+}
+
+section h4 {
+  color: white;
+  text-shadow: 1px 1px black;
+  padding: 2px;
+  margin-left: 15px;
+}
+
+.pokemon-description h4 {
+  margin: 0;
+  padding-bottom: 8px;
+  margin-left: 15px;
+}
+
+p {
+  margin: 0;
+  padding-bottom: 8px;
+  color: white;
+  text-shadow: 1px 1px black;
+  margin-left: 15px;
+  margin-right: 15px;
+}
+
+section h5 {
+  color: white;
+  text-shadow: 1px 1px black;
+  padding: 2px;
+  margin-top: 5px;
+  margin-left: 15px;
+  margin-right: 15px;
+}
+
+.pokemon-name input {
+  padding: 10px;
+  margin-left: 15px;
+  margin-bottom: 5px;
+  margin-top: 5px;
+}
+
+.pokemon-image input {
+  padding: 0 10px;
+  margin-left: 20px;
+  margin-bottom: 5px;
+  margin-top: 5px;
+}
+
+.pokemon-no input,
+.pokemon-type input,
+.pokemon-hp input,
+.pokemon-attack input,
+.pokemon-defense input,
+.pokemon-spatk input,
+.pokemon-spdef input,
+.pokemon-speed input,
+.pokemon-generation input,
+.pokemon-legendary input {
+  margin-right: 15px;
+  text-align: end;
+  width: 50px;
+  align-items: center;
+}
+
+.pokemon-type-select {
+  align-items: center;
+  margin-right: 15px;
+}
+
+.pokemon-description textarea {
+  width: 90%;
+  margin-left: 15px;
 }
 </style>
